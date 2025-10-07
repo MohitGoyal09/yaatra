4adc0ba408bb4922ec88263c481e86e44325b1f5
Mohit <goel0632@gmail.com>
2025-10-07 10:12:33 +0530

    Remove unused debug, demo actions, social test, and test API pages to streamline the application structure.

--- app/[locale]/social-test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MockEvents } from '@/components/social/mock-events';
-
-export default function SocialTestPage() {
-  return (
-    <div className="container mx-auto py-8 px-4">
-      <MockEvents />
-    </div>
-  );
-}
